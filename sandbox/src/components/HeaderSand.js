9e48b051b9faded61dee4ad773ed0efdb39f57c2
Ponomarenko.Oleg <ponomarenko147@gmail.com>
2018-07-12 13:20:42 +0300

    css and back

--- sandbox/src/components/HeaderSand.js
+++ sandbox/src/components/HeaderSand.js
@@ -9,7 +9,7 @@ export default class HeaderSand extends Component {
           <header className="App-header App">
           <h1 className="App-title">Welcome to the Sandbox!</h1>
           <input type='text' placeholder='text' name="input" value='Type your name' />
-          <button type='button'>+</button><br /><br />
+          <button type='button'>Save</button><br /><br />
           <span>Your Link there  	&nbsp; </span>
           <Link to="http://localhost:3001/">http://localhost:3001/</Link> 
         </header>
